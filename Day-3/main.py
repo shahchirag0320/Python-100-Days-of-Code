5c66b22411bbd35f75bc81474fa9e2e8bd0162ea
shahchirag0320 <shahchirag0320@gmail.com>
2022-12-01 01:43:27 +0530

    updated main.py in DAY-3

--- Day-3/main.py
+++ Day-3/main.py
@@ -4,3 +4,6 @@ import hashlib
 # Read and work with a file named 'words.csv'
 df = pandas.read_csv('one.csv')
 print(df) # This will display first few rows from the words.csv file
+
+m = hashlib.sha256()
+print(m)
