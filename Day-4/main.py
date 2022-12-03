70690492328718b169a9cb784fb5fa9095d8163c
shahchirag0320 <shahchirag0320@gmail.com>
2022-12-04 01:54:31 +0530

    updated git repo for practise

--- Day-4/main.py
+++
@@ -1,9 +0,0 @@
-print("Hello world")
-print(50)
-print("Bye")
-print(17*13)
-
-# quiz
-
-# print("--- Your poem here ---")
-print("A heap of broken images, where the sun beats, And the dead tree gives no shelter, the cricket no relief, And the dry stone no sound of water. Only")
