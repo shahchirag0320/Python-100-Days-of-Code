250dfe735dd785f50536fdd8fe1567f5dee84d20
shahchirag0320 <shahchirag0320@gmail.com>
2022-12-05 05:14:55 +0530

    practise-1 day-4 and day-5

---
+++ Day-4/main.py
@@ -0,0 +1,8 @@
+print("Hello World", 7)
+print(5)
+print("Bye")
+print(17*13)
+
+# Quiz
+# print("---Your poem here---")
+print("Two roads diverged in a yellow wood, And sorry I could not travel both And be one traveler, long I stood To where it bent in the undergrowth;")
--- Day-5/main.py
+++ Day-5/main.py
@@ -1,2 +1,32 @@
+# Hey Harry please dont remove this line
+# Author: Harry
+# Course: 100DaysofCode
 
+# Escape sequence
+print("Hey I am a good boy \nand this viewer is also a good boy/girl")
 
+print("Hello world")
+
+#This is a 'Single-Line Comment'
+print("This is a print statement.")
+
+print("Hello world!!") #Printing Hello world
+
+# It will execute a block of code if a specified condition is true.
+# If the condition is false then it will execute another block of code.
+p = 7
+if (p > 5):
+    print("p is greater than 5.")
+else:
+    print("p is not greater than 5.")
+
+"""
+This is an if-else statement.
+It will execute a block of code if a specified condition is true.
+If the condition is false then it will execute another block of code.
+"""
+p = 7
+if (p > 5):
+    print("p is greater than 5.")
+else:
+    print("p is not greater than 5.")
