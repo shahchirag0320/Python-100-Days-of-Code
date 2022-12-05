78999c9509a844e947cf5f576fa9aff8430d4e87
shahchirag0320 <shahchirag0320@gmail.com>
2022-12-06 03:02:42 +0530

    adding Day-6 code

--- Day-5/main.py
+++ Day-5/main.py
@@ -10,23 +10,32 @@ print("Hello world")
 #This is a 'Single-Line Comment'
 print("This is a print statement.")
 
-print("Hello world!!") #Printing Hello world
+# print("Hello world!!") #Printing Hello world
 
 # It will execute a block of code if a specified condition is true.
 # If the condition is false then it will execute another block of code.
-p = 7
-if (p > 5):
-    print("p is greater than 5.")
-else:
-    print("p is not greater than 5.")
+# p = 7
+# if (p > 5):
+#     print("p is greater than 5.")
+# else:
+#     print("p is not greater than 5.")
 
 """
 This is an if-else statement.
 It will execute a block of code if a specified condition is true.
 If the condition is false then it will execute another block of code.
 """
-p = 7
-if (p > 5):
-    print("p is greater than 5.")
-else:
-    print("p is not greater than 5.")
+# p = 7
+# if (p > 5):
+#     print("p is greater than 5.")
+# else:
+#     print("p is not greater than 5.")
+
+"""
+Escape Sequence Characters
+"""
+
+# print("This doesnt "execute")
+print("This will \"execute\" ")
+
+print("Hey", 6, 7, sep="~")
--- Day-6/main.py
+++ Day-6/main.py
@@ -1,2 +1,23 @@
+a = 1
+b = True
+c = "Harry"
+d = None
+print(a)
+a1 = 1
+print(a + a1)
+print("The type of a is ", type(a))
+print("The type of b is ", type(b))
+print("The type of c is ", type(c))
 
+# List
+list1 = [8, 2.3, [-4, 5], ["apple", "banana"]]
+print(list1)
+
+# Tuple
+tuple1 = (("parrot", "sparrow"), ("Lion", "Tiger"))
+print(tuple1)
+
+# Dictionary
+dict1 = {"name":"Sakshi", "age":20, "canVote":True}
+print(dict1)
 
