9368f1074346f82a443ed13d49fba1f164e928d9
shahchirag0320 <shahchirag0320@gmail.com>
2022-12-07 00:58:23 +0530

    added Day-8 and Day-9 folder

--- Day-7/main.py
+++ Day-7/main.py
@@ -1,2 +1,17 @@
+# print(5+6)
+# print(15-6)
+# print(15*6)
+# print(15/6)
+# print(15//6)
+# print(5 % 3)
+# print(5**3)
 
 
+# Excercise
+
+a = 10
+b = 5
+print("The sum of", a, "and", b, "is", a + b)
+print("The subtraction of", a, "and", b, "is", a - b)
+print("The multiplication of", a, "and", b, "is", a * b)
+print("The division of", a, "and", b, "is", a // b)
