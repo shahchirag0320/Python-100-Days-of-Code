1e660fee3cf6d4b0bf002bf9d702b63660ceb5fb
shahchirag0320 <shahchirag0320@gmail.com>
2022-12-07 02:25:28 +0530

    Completing practise of day-7

---
+++ Day-8/main.py
@@ -0,0 +1,10 @@
+# Create a calculator capable of performing addition, subtraction, multiplication and division operations on two numbers.
+# Your program should format the output in a readable manner!
+
+a = 10
+b = 3
+
+print("The sum of", a, "and", b, "is", a+b)
+print("The subtraction of", a, "and", b, "is", a-b)
+print("The multiplication of", a, "and", b, "is", a*b)
+print("The division of", a, "and", b, "is", a/b)
