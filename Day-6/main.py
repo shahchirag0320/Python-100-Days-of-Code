14b3734c7cba4c1beb338847d00d34c5d97f2833
shahchirag0320 <shahchirag0320@gmail.com>
2022-12-07 01:28:20 +0530

    working on day-6

---
+++ Day-6/main.py
@@ -0,0 +1,12 @@
+a = 1 #int, float
+b = True
+c = "Harry"
+d = None
+print(a)
+print(b)
+a1 = 9
+print(a + a1)
+print("The type of a is ", type(a))
+print("The type of b is", type(b))
+print("The type of c is ", type(c))
+
