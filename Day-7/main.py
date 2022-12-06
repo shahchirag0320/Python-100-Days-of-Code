c62dfa06e64a4f35c1253b2adef6113c8807f380
shahchirag0320 <shahchirag0320@gmail.com>
2022-12-07 02:27:06 +0530

    Fixing day-7 excercise code

--- Day-7/main.py
+++ Day-7/main.py
@@ -5,3 +5,15 @@ print(15/6)
 print(15//6)
 print(5%3)
 print(2**3)
+
+# Excercise
+# Create a calculator capable of performing addition, subtraction, multiplication and division operations on two numbers.
+# Your program should format the output in a readable manner!
+
+a = 10
+b = 3
+
+print("The sum of", a, "and", b, "is", a+b)
+print("The subtraction of", a, "and", b, "is", a-b)
+print("The multiplication of", a, "and", b, "is", a*b)
+print("The division of", a, "and", b, "is", a/b)
