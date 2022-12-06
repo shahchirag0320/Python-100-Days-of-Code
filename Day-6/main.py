5e86985650dc87fc17a26b149202830f94d08a94
shahchirag0320 <shahchirag0320@gmail.com>
2022-12-07 01:37:19 +0530

    completing day-6 practise

--- Day-6/main.py
+++ Day-6/main.py
@@ -1,6 +1,7 @@
-a = 1 #int, float
-b = True
-c = "Harry"
+a = 1  # int
+# a = 1.1  #float
+b = False  # bool
+c = "Harry"  # str
 d = None
 print(a)
 print(b)
@@ -10,3 +11,14 @@ print("The type of a is ", type(a))
 print("The type of b is", type(b))
 print("The type of c is ", type(c))
 
+#list
+list1 = [8, 2.3, [-4, 5], ["apple", "banana"]]
+print(list1)
+
+#tuple
+tuple1 = (("parrot", "sparrow"), ("Lion", "Tiger"))
+print(tuple1)
+
+#dict - collection of key-value pairs
+dict1 = {"name":"Sakshi", "age":20, "canVote":True}
+print(dict1)
